b11297894d83702416c81eba3cff788902c47f58
PIYUSH SINGH <singhpiyush1403@gmail.com>
2024-12-15 16:01:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+class Main{
+    int a1= 10;
+    int b2= 30;
+    public static void main(String args[]){
+        Main myObj = new Main();
+        System.out.println(myObj.a1);
+        System.out.println(myObj.b2);
+    }
+}
+
+// class Main{
+//     int x = 20;
+//     public static void main(String args[]){
+//         Main myObj = new Main();
+//         myObj.x = 23;
+//         System.out.println(myObj.x);
+//     }
+// }
+
+// class animal{
+//     String color;
+//     int age;
+//     public static void main(String args[]){
+//         animal kumar = new animal();
+//         kumar.color= "black";
+//         kumar.age = 20;
+//         System.out.println(kumar.color+" "+kumar.age);
+        
+//     }
+// }
